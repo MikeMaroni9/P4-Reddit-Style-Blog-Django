6738cdcbdebd0caa425e12c414ed04ce1ef3a0d5
MikeMaroni9 <k.vaitka@gmail.com>
2023-08-31 18:10:11 +0000

    allauth templates added to project template folder

--- p4reddit/settings.py
+++ p4reddit/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'p4reddit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
